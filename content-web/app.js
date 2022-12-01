0182a880ed7e0b083f549a0f768ad3df1cba383d
adrian3s <adrian.alpizar@outlook.com>
2022-12-01 21:33:40 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("6cc0fceb-74f1-42f5-a63d-d2c89bd0846d");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
